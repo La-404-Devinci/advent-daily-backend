afbdca0f66e09bc90fbb845470f58a2827dbbbca
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-10-30 00:48:42 +0100

    chore: link routes

--- routes/auth/router.ts
+++ routes/auth/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import Route_Auth_Sendmail from "./send-mail";
+import Route_Auth_Login from "./login";
 
 const authRouter = Router();
 
-authRouter.post("/send-mail");
-authRouter.post("/login");
+authRouter.post("/send-mail", Route_Auth_Sendmail);
+authRouter.post("/login", Route_Auth_Login);
 authRouter.get("/me");
 authRouter.get("/me/qr");
 
