1c10e47b762cf5a8fa01360f722c275f514eecb0
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-10-30 18:42:48 +0100

    fix: invert admin check condition

--- middlewares/auth/admin.ts
+++ middlewares/auth/admin.ts
@@ -3,11 +3,11 @@ import Status from "@/models/status";
 import { NextFunction, Request, Response } from "express";
 
 export const isAdmin = (req: Request) => {
-    return req.header("X-Admin-Key") !== globals.env.ADMIN_TOKEN;
+    return req.header("X-Admin-Key") === globals.env.ADMIN_TOKEN;
 };
 
 export default function middlewareAdmin(req: Request, res: Response, next: NextFunction) {
-    if (isAdmin(req)) {
+    if (!isAdmin(req)) {
         return Status.send(req, next, {
             status: 401,
             error: "errors.auth.admin"
