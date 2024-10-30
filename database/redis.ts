22f0c64da6f8b0cb35ab925ff23ea76acb86155e
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-10-30 01:13:13 +0100

    fix: redis client

--- database/redis.ts
+++ database/redis.ts
@@ -1,28 +1,37 @@
 import globals from "@/env/env";
-import redis from "redis";
+import Logger from "@/log/logger";
+import { createClient } from "redis";
 
-const client = redis.createClient({
-    url: `redis://${globals.env.REDIS_HOST}:${globals.env.REDIS_PORT}`
-});
+export default abstract class Redis {
+    public static client: ReturnType<typeof createClient>;
 
-const Redis = {
-    async get<T>(key: string): Promise<T | null> {
-        const value = await client.get(key);
+    public static init() {
+        Redis.client = createClient({
+            url: `redis://${globals.env.REDIS_HOST}:${globals.env.REDIS_PORT}`
+        });
+
+        Redis.client.on("error", (err) => {
+            Logger.error(err);
+        });
+
+        Redis.client.connect();
+    }
+
+    public static async get<T>(key: string): Promise<T | null> {
+        const value = await Redis.client.get(key);
         return value ? JSON.parse(value) : null;
-    },
+    }
 
-    async set<T>(key: string, value: T, ttl?: number): Promise<void> {
+    public static async set<T>(key: string, value: T, ttl?: number): Promise<void> {
         const stringValue = JSON.stringify(value);
         if (ttl) {
-            await client.setEx(key, ttl, stringValue);
+            await Redis.client.setEx(key, ttl, stringValue);
         } else {
-            await client.set(key, stringValue);
+            await Redis.client.set(key, stringValue);
         }
-    },
-
-    async del(key: string): Promise<void> {
-        await client.del(key);
     }
-};
 
-export default Redis;
+    public static async del(key: string): Promise<void> {
+        await Redis.client.del(key);
+    }
+}
