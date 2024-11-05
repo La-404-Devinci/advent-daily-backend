d88aa6db77a8e39484cfc4214d18e04327c03a63
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-11-05 18:44:02 +0100

    feat: added /admin/dump endpoint

---
+++ routes/admin/dump.ts
@@ -0,0 +1,27 @@
+import DB from "@/database/config";
+import { acquired } from "@/database/schema/acquired";
+import { challenges } from "@/database/schema/challenges";
+import { clubs } from "@/database/schema/clubs";
+import { granters } from "@/database/schema/granters";
+import { users } from "@/database/schema/users";
+import Status from "@/models/status";
+import { Request, Response, NextFunction } from "express";
+
+export default async function Route_Admin_Dump(req: Request, res: Response, next: NextFunction) {
+    const dumpAcquired = await DB.instance.select().from(acquired);
+    const dumpChallenges = await DB.instance.select().from(challenges);
+    const dumpClubs = await DB.instance.select().from(clubs);
+    const dumpGranters = await DB.instance.select().from(granters);
+    const dumpUsers = await DB.instance.select().from(users);
+
+    return Status.send(req, next, {
+        status: 200,
+        data: {
+            acquired: dumpAcquired,
+            challenges: dumpChallenges,
+            clubs: dumpClubs,
+            granters: dumpGranters,
+            users: dumpUsers
+        }
+    });
+}
