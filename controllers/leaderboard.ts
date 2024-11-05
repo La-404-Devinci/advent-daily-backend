deedf10eded9b5a20a5803e4d65ce33984b9b40a
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-11-05 17:08:57 +0100

    feat: add club leaderboard

--- controllers/leaderboard.ts
+++ controllers/leaderboard.ts
@@ -5,6 +5,9 @@ import { challenges } from "@/database/schema/challenges";
 import { users } from "@/database/schema/users";
 import Logger from "@/log/logger";
 import { eq, sum } from "drizzle-orm";
+import UserController from "./users";
+import { clubs } from "@/database/schema/clubs";
+import ClubController from "./clubs";
 
 export default abstract class LeaderboardController {
     public static async getLeaderboardEtag() {
@@ -29,7 +32,22 @@ export default abstract class LeaderboardController {
         }));
     }
 
+    public static async getClubLeaderboard() {
+        const leaderboardUuids = await Redis.sortedAll<number>(`leaderboard:clubs`);
+        const allClubs = await ClubController.getAllClubs();
+
+        const clubsMap = new Map(allClubs.map((club) => [club.id, club]));
+        return leaderboardUuids.map((set) => ({
+            score: set.score,
+            club: clubsMap.get(set.value)
+        }));
+    }
+
     public static async grant(userUuid: string, challengeId: number) {
+        // Check if the user exists
+        const user = await UserController.getUser(userUuid);
+        if (!user) return false;
+
         // Create a new acquired record
         try {
             await DB.instance.insert(acquired).values({
@@ -54,6 +72,29 @@ export default abstract class LeaderboardController {
 
         // Add/update the user's score in the leaderboard
         await Redis.sortedSet("leaderboard:users", userScore, userUuid);
+
+        if (user.clubId) {
+            // Add/update the club's score in the leaderboard
+
+            //! Not sure if this is the MORE efficient way but,
+            // every granter is a physical person. so they can't
+            // grant a lot of challenges per second and overload
+            // the server. That said, it's not that bad.
+
+            const clubScoreRequest = await DB.instance
+                .select({
+                    score: sum(challenges.score).as("score")
+                })
+                .from(clubs)
+                .innerJoin(users, eq(clubs.id, users.clubId))
+                .innerJoin(acquired, eq(users.uuid, acquired.userUuid))
+                .where(eq(clubs.id, user.clubId));
+
+            if (clubScoreRequest.length !== 1) return false;
+            const clubScore = parseInt(clubScoreRequest[0].score ?? "0");
+
+            await Redis.sortedSet(`leaderboard:clubs`, clubScore, user.clubId);
+        }
 
         // Add ETag to the user's leaderboard
         await Redis.set(`leaderboard:etag`, crypto.randomUUID());
