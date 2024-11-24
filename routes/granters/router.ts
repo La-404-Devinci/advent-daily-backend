278f2b480c302ddf329967c0ee9e76beca732705
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-11-24 12:00:51 +0100

    feat: added ungrant endpoint

--- routes/granters/router.ts
+++ routes/granters/router.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import Route_Granters_Login from "./login";
 import Route_Granters_Grant from "./grant";
 import Route_Granters_Me from "./me";
+import Route_Granters_Ungrant from "./ungrant";
 
 const grantersRouter = Router();
 
 grantersRouter.get("/me", Route_Granters_Me);
 grantersRouter.post("/login", Route_Granters_Login);
 grantersRouter.post("/grant", Route_Granters_Grant);
+grantersRouter.post("/ungrant", Route_Granters_Ungrant);
 
 export default grantersRouter;
